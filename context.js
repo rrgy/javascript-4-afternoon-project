8fd33e1603c09abecf867cf66c045cacfb721d68
Ryan Morgan <rrgy80@gmail.com>
2019-09-19 16:08:30 -0600

    finished

--- context.js
+++ context.js
@@ -22,6 +22,15 @@
 //Code Here
 
 
+let user = {
+  username: '',
+  email: '',
+  getUsername(){
+    return user.username
+  }
+  
+}
+
 
 ////////// PROBLEM 2 //////////
 
@@ -42,7 +51,7 @@ class Animal {
 }
 
 //Code Here
-
+ let animal1 = new Animal
 
 
 ////////// PROBLEM 3 //////////
@@ -62,7 +71,7 @@ let who = {
 }
 
 //Code Here
-
+let whoSaysHi = sayHi.bind(who)
 
 
 ////////// PROBLEM 4 //////////
@@ -76,7 +85,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = this
 
 let product = {
   name: 'snake plant',
@@ -87,7 +96,8 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product
+
 
 let vacation = {
   location: 'Hawaii',
@@ -98,7 +108,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -116,4 +126,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
